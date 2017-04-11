dde20905006a2cfbbf8f413988e92d42196d7fa9
zhaotoday <729234283@qq.com>
2017-04-11 22:23:38 +0800

    优化

--- rest.js
+++ rest.js
@@ -1,58 +1,84 @@
-/**
- * @file 基于 axios 的 RESTful HTTP 简单封装。
- * @author 赵金添 <729234283@qq.com>
- */
-
 import axios from 'axios'
 
 /**
- * @class REST 接口请求
+ * 基于 axios 的 RESTful HTTP 简单封装
+ * @author 赵金添 <729234283@qq.com>
  */
 export default class REST {
   /**
-   * 构造函数
+   * 构造方法
    */
-  constructor() {
-    // 接口基础地址
+  constructor () {
+    /**
+     * 接口基础地址
+     * @type {string}
+     */
     this.baseURL = ''
-    // 接口版本
+
+    /**
+     * 接口版本
+     * @type {string}
+     */
     this.version = ''
-    // 请求路劲
+
+    /**
+     * 请求路劲
+     * @type {string}
+     */
     this.path = ''
-    // headers
+
+    /**
+     * Headers
+     * @type {Object}
+     */
     this.headers = {}
+
+    // 支持的请求方式
+    const methods = ['GET', 'POST', 'PATCH', 'PUT', 'DELETE']
+
+    // 注册方法到 this
+    methods.forEach((method) => {
+      this[method] = options => this._request(method, options)
+    })
   }
 
   /**
    * 请求
-   * @param {string} method - 请求方式
-   * @param {object} options - 选项
-   * @return {object}
+   * @param {string} method='GET' 请求方式
+   * @param {Object} [options={}] 选项
+   * @param {string} [options.uri=''] 资源唯一标示，一般是 ID
+   * @param {Object} [options.params=null] GET 参数
+   * @param {Object} [options.data=null] POST/PUT/PATCH 数据
+   * @return {Object}
    */
-  _request(method = 'GET', options = {}) {
+  _request (method = 'GET', options = {}) {
+    const {uri = '', params = null, data = null} = options
     let url = this.version ? `/${this.version}/${this.path}` : `/${this.path}`
-    const headers = Object.keys(this.headers) ? {headers: this.headers} : {}
+
+    if (uri) {
+      url = `${url}/${uri}`
+    }
 
     // GET
-    if (options.params) {
-      url = url + this._objToUrl(options.params)
+    if (params) {
+      url = url + this._objToUrl(params)
     }
 
     return axios({
-      ...headers,
-      method: method,
       baseURL: this.baseURL,
-      url: url,
-      data: options.data || {}
+      headers: this.headers,
+      method,
+      url,
+      data
     })
   }
 
   /**
    * 对象转 URL
-   * @param {object} obj - 待转化对象
+   * @param {Object} obj 待转化对象
    * @return {string}
    */
-  _objToUrl(obj) {
+  _objToUrl (obj) {
     if (!obj || !Object.keys(obj).length) {
       return ''
     }
@@ -64,19 +90,19 @@ export default class REST {
 
   /**
    * 附加路劲
-   * @param {string} path - 路劲
+   * @param {string} [path=''] 路劲
    */
-  addPath(path = '') {
-    this.path = this.path + path
+  addPath (path = '') {
+    this.path = this.path + '/' + path
 
     return this
   }
 
   /**
-   * 添加 headers
-   * @param {object} headers - headers
+   * 添加 Headers
+   * @param {Object} headers Headers
    */
-  addHeaders(headers) {
+  addHeaders (headers) {
     this.headers = {
       ...this.headers,
       ...headers
@@ -86,50 +112,14 @@ export default class REST {
   }
 
   /**
-   * path 参数替换
-   * @param {object} options - path 参数列表
+   * 路劲参数替换
+   * @param {Object} options={} 路劲参数列表
    */
-  replace(options = {}) {
+  replace (options = {}) {
     Object.keys(options).forEach((key) => {
       this.path = this.path.replace(new RegExp('{' + key + '}', 'img'), options[key])
     })
 
     return this
-  }
-
-  /**
-   * GET
-   * @param {object} options - 选项
-   * @returns {object}
-   */
-  GET(options = {}) {
-    return this._request('GET', options)
-  }
-
-  /**
-   * DELETE
-   * @param {object} options - 选项
-   * @returns {object}
-   */
-  DELETE(options = {}) {
-    return this._request('DELETE', options)
-  }
-
-  /**
-   * POST
-   * @param {object} options - 选项
-   * @returns {object}
-   */
-  POST(options = {}) {
-    return this._request('POST', options)
-  }
-
-  /**
-   * PATCH
-   * @param {object} options - 选项
-   * @returns {object}
-   */
-  PATCH(options = {}) {
-    return this._request('PATCH', options)
   }
 }
